7124d864b88e28ef22fb3db39f117d33beb0c651
Richard <richard.lowrey@ufl.edu>
2017-12-13 17:22:44 -0500

    Added emulate.sh

---
+++ Code/emulate.sh
@@ -0,0 +1,12 @@
+module load aocl/17.0
+aoc -v -march=emulator device/$1.cl -o bin/$1.aocx
+if [ $? -ne 0 ]
+then
+  exit $?
+fi
+make
+if [ $? -ne 0 ]
+then
+  exit $?
+fi
+env CL_CONTEXT_EMULATOR_DEVICE_ALTERA=1 ./bin/host
