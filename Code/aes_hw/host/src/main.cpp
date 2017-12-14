c1102b0b7608d1a3433fa51aa162b5fc20e03e96
Richard <richard.lowrey@ufl.edu>
2017-12-14 04:31:50 -0500

    Modified code

--- Code/aes_hw/host/src/main.cpp
+++ Code/aes_hw/host/src/main.cpp
@@ -116,7 +116,7 @@ int main() {
   fp2 = fopen("text.txt","w");
   fseek(fp,0,SEEK_END);
   size_t file_size = ftell(fp);
-
+  rewind(fp);
   uint8_t *output = (uint8_t *)malloc(sizeof(uint8_t)*16);
   uint8_t key[16]   = {0x00,0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x08,0x09,0x0a,0x0b,0x0c,0x0d,0x0e,0x0f};
   uint8_t input[16];
