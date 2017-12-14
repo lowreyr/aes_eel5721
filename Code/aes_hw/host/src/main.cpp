824aaa5f8a3764316e591569f676ea5bcc13a163
Richard <richard.lowrey@ufl.edu>
2017-12-13 23:55:12 -0500

    Modified code

--- Code/aes_hw/host/src/main.cpp
+++ Code/aes_hw/host/src/main.cpp
@@ -105,7 +105,7 @@ int main(int argc, char *argv[]) {
 
   uint8_t *output = (uint8_t *)malloc(sizeof(uint8_t)*16);
   uint8_t key[16]   = {0x00,0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x08,0x09,0x0a,0x0b,0x0c,0x0d,0x0e,0x0f};
-  char input[16];
+  char input[16] = {0x00,0x11,0x22,0x33,0x44,0x55,0x66,0x77,0x88,0x99,0xaa,0xbb,0xcc,0xdd,0xee,0xff};
 
   cl_int status;
 
@@ -137,11 +137,11 @@ int main(int argc, char *argv[]) {
   fp = fopen("hello.txt", "r");
   fp2 = fopen("text.txt","w");
 
-  for(int i = 0; i < 16; i++)
-  {
-    input[i] = fgetc(fp);
-    printf("%c",input[i]);
-  }
+  // for(int i = 0; i < 16; i++)
+  // {
+  //   input[i] = fgetc(fp);
+  //   printf("%c",input[i]);
+  // }
 
   const double start_time = getCurrentTimestamp();
   // Launch the kernel
