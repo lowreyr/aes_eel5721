9617ef56c34874ef26c7b5a39a5efbe33111634f
Richard <richard.lowrey@ufl.edu>
2017-12-13 22:38:47 -0500

    Modified code

--- Code/aes_hw/host/src/main.cpp
+++ Code/aes_hw/host/src/main.cpp
@@ -277,7 +277,7 @@ bool init() {
   checkError(status, "Failed to create command queue");
 
   // Create the program.
-  std::string binary_file = getBoardBinaryFile("hello_aes", device);
+  std::string binary_file = getBoardBinaryFile("aes", device);
   printf("Using AOCX: %s\n", binary_file.c_str());
   program = createProgramFromBinary(context, binary_file.c_str(), &device, 1);
 
