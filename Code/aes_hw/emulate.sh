33033b1448d33808cb7d1e264873e783efaf7112
Richard <richard.lowrey@ufl.edu>
2017-12-13 23:08:25 -0500

    Modified code

--- Code/aes_hw/emulate.sh
+++ Code/aes_hw/emulate.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 git pull
-module load aocl/17.0
-aoc -v -march=emulator device/$1.cl -o bin/$1.aocx
-if [ $? -ne 0 ]
-then
-  exit $?
-fi
+#module load aocl/17.0
+#aoc -v -march=emulator device/$1.cl -o bin/$1.aocx
+# if [ $? -ne 0 ]
+# then
+#   exit $?
+# fi
 make
 if [ $? -ne 0 ]
 then
